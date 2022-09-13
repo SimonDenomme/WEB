7588b460870ae27960570a23d0ed474c9162eff7
Jonathan Trottier <jotrotier22@gmail.com>
2022-09-12 21:18:02 -0400

    projet refait au GRAND complet

---
+++ MiniStore - Copie/MiniStore/Models/ModelMini.cs
@@ -0,0 +1,20 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace MiniStore.Models
+//{
+//    public class ModelMini
+//    {
+//        public int Id { get; set; }
+//        public int Name { get; set; }
+//        public double Price { get; set; }
+//        public string ImagePath { get; set; }
+//        public List<Review> Reviews { get; set; }
+
+
+
+
+//    }
+//}
---
+++ MiniStore - Copie/MiniStore/ViewComponents/ListeProduitsVedettes.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniStore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniStore.ViewComponents
+{
+    public class ListeProduitsVedettes : ViewComponent
+    {
+        private readonly MiniStoreContext _context;
+
+        public ListeProduitsVedettes(MiniStoreContext context)
+        {
+            _context = context;
+        }
+
+        //public async Task<IViewComponentResult> InvokeAsync(
+        //    DateTime? dueBefore = null,
+        //    bool showCompleted = true,
+        //    int minPriority = 0)
+        //{
+        //    var toDos = await _context.Todos
+        //        .Where(toDo => (showCompleted || !toDo.IsDone) &&
+        //                                    toDo.Priority >= minPriority &&
+        //                                    (dueBefore == null || toDo.DueDate <= dueBefore))
+        //        .ToListAsync();
+
+        //    return View(toDos);
+        //}
+
+
+
+    }
+}
