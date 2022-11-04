fe333ac0e5aa52c9147c13c7a643366258bb3f5b
SimonDenomme <simon.denomme@hotmail.com>
2022-11-04 11:40:04 -0400

    build issu fix

--- MiniStore/MiniStore/Domain/Command.cs
+++ MiniStore/MiniStore/Domain/Command.cs
@@ -1,8 +1,4 @@
-﻿using MiniStore.Entity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace MiniStore.Domain
 {
