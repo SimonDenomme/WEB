e000ca2c7ac242bc5cc3fe4ee93fce5961863a7b
Jonathan Trottier <jotrotier22@gmail.com>
2022-11-04 11:03:41 -0400

    commandes

---
+++ MiniStore/MiniStore/Controllers/CommandController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MiniStore.Data;
+using MiniStore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniStore.Controllers
+{
+    public class CommandController : Controller
+    {
+        private readonly MiniStoreContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public CommandController(MiniStoreContext context,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> CommandForm()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ConfirmCommand()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> CancelCommand()
+        {
+            return View();
+        }
+        public async Task<IActionResult> CreateCommand()
+        {
+
+
+            return View();
+        }
+
+    }
+}
---
+++ MiniStore/MiniStore/Domain/Command.cs
@@ -0,0 +1,24 @@
+﻿using MiniStore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniStore.Domain
+{
+    public class Command
+    {
+        public int Id { get; set; }
+        //public int  { get; set; }
+
+        public int UserId { get; set; }
+        public decimal TotalAvantTaxe { get; set; }
+        public decimal Taxe { get; set; }
+        public decimal TotalApresTaxe { get; set; }
+        public decimal FraisLivraison { get; set; }
+
+
+        public List<ItemInCart> Items { get; set; }
+
+    }
+}
