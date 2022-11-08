1c0b658838709e4a17278a56af41f95b9a659288
Dushelle Mercedes Siandja Nana <1921181@etu.cegepsth.qc.ca>
2022-11-08 03:52:57 -0500

    listes des commandes

---
+++ MiniStore/MiniStore/Models/ClientViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MiniStore.Domain;
+using System.Collections.Generic;
+
+namespace MiniStore.Models
+{
+    public class ClientViewModel
+    {
+        public ApplicationUser User { get; set; }
+
+        //public string Name { get; set; }
+
+        public List<Commande> Commands { get; set; }
+
+    }
+}
